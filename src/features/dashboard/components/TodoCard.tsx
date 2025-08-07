1dfe2967282a3fc26554a0ceb114dd31a48dca11
roel <ash49901@gmail.com>
2025-08-07 22:20:29 +0900

    [FEAT] Task 더보기 DropDown 구현

--- src/features/dashboard/components/TodoCard.tsx
+++ src/features/dashboard/components/TodoCard.tsx
@@ -2,6 +2,7 @@
 
 import { Checkbox } from '@/components/ui/checkbox';
 import { cn } from '@/lib/utils';
+import CommonDropdown from "@/components/ui/dropdown/CommonDropdown";
 
 type Task = {
     id: number;
@@ -66,12 +67,54 @@ export default function TodoCard({
             </div>
             {/* 오른쪽: 설명 및 링크칩 */}
             <div className="flex flex-col min-w-[230px] max-w-xs pt-2">
-                {/* ic-more를 우측 상단에 */}
-                <img
-                    src="/icons/ic-more.svg"
-                    alt="더보기"
-                    className="self-end w-4 h-4 mb-2 cursor-pointer"
-                />
+                {/* ic-more를 우측 상단에 완전히 붙이기 위한 flex 컨테이너 */}
+                <div className="flex justify-end items-start w-full">
+                    <CommonDropdown
+                        align="right"
+                        offsetY={0}
+                        dropdownClassName="min-w-[100px]"
+                        renderButton={({ onClick }) => (
+                            <button type="button" onClick={onClick} className="p-0 m-0">
+                                <img
+                                    src="/icons/ic-more.svg"
+                                    alt="더보기"
+                                    className="w-4 h-4 cursor-pointer"
+                                />
+                            </button>
+                        )}
+                    >
+                        <div className="min-w-[180px] flex flex-col p-[8px]">
+                            <button
+                                type="button"
+                                className="group flex items-center pl-4 pr-2 py-[6px] rounded-[6px] hover:bg-coolNeutral-200 transition justify-between label-1 text-coolNeutral-700 font-semibold"
+                                onClick={() => {
+                                    console.log("수정하기")
+                                }}
+                            >
+                                수정하기
+                                <img
+                                    src="/icons/ic-arrow-right.svg"
+                                    alt=">"
+                                    className="w-4 h-4 ml-2 opacity-0 group-hover:opacity-100 transition-opacity"
+                                />
+                            </button>
+                            <button
+                                type="button"
+                                className="group flex items-center pl-4 pr-2 py-[6px] rounded-[6px] hover:bg-coolNeutral-200 transition justify-between label-1 text-coolNeutral-700 font-semibold"
+                                onClick={() => {
+                                    console.log("삭제하기")
+                                }}
+                            >
+                                삭제하기
+                                <img
+                                    src="/icons/ic-arrow-right.svg"
+                                    alt=">"
+                                    className="w-4 h-4 ml-2 opacity-0 group-hover:opacity-100 transition-opacity"
+                                />
+                            </button>
+                        </div>
+                    </CommonDropdown>
+                </div>
                 <div className="caption-1 font-semibold text-coolNeutral-600 mb-6 whitespace-pre-line">
                     {description}
                 </div>
