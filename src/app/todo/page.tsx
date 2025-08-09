1b3bdecaeb6041755248dab59caadafa9aabce5b
roel <ash49901@gmail.com>
2025-08-09 21:50:34 +0900

    [FEAT] 필요없는 코드 제거

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -6,9 +6,6 @@ import CommonDropdown from "@/components/ui/dropdown/CommonDropdown";
 import {usePathname, useRouter} from 'next/navigation';
 import {useMemo, useState} from "react";
 import AddTaskModal from "@/features/dashboard/components/AddTaskModal";
-import Image from "next/image";
-import NotificationDropdown from "@/components/ui/dropdown/NotificationDropdown";
-import AccountDropdown from "@/components/ui/dropdown/AccountDropdown";
 
 import { DragOverlay, DndContext, DragStartEvent, DragEndEvent } from "@dnd-kit/core";
 import { SortableContext, verticalListSortingStrategy, arrayMove } from "@dnd-kit/sortable";
@@ -363,32 +360,5 @@ function TaskHeaderBar() {
 
             <ChatSideModal open={chatModalOpen} onClose={() => setChatModalOpen(false)} tabs={tabs} chats={chats}/>
         </div>
-    );
-}
-
-function Header() {
-    const pathname = usePathname();
-    const isOnboarding = pathname.startsWith("/onboarding");
-
-    if (isOnboarding) return null;
-
-    return (
-        <header className="flex items-center w-full px-8 py-4 bg-white">
-            {/* 좌측: 워크스페이스/날짜 */}
-            <div className="flex items-center gap-4 flex-1 min-w-0">
-                {/* 워크스페이스명 + 아이콘 */}
-                <div className="flex items-center gap-2">
-                    {/* 칸반 아이콘(예시) */}
-                    <Image src="/icons/ic-side-bar.svg" alt="" width={16} height={16} />
-                    <span className="label-1 text-coolNeutral-900 truncate">{'{User}’s Workspace'}</span>
-                </div>
-            </div>
-
-            {/* 우측: 알림, 프로필, 로그아웃 */}
-            <div className="flex items-center gap-2 ml-6">
-                <NotificationDropdown/>
-                <AccountDropdown />
-            </div>
-        </header>
     );
 }
